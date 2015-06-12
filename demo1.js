26b87875e81413d24644c1b853940289c6cc5f8e
biswa-13 <bichhubiswa@gmail.com>
2015-06-12 11:58:52 +0530

    Chnages Made to add the Desprite functionality
    Chnages Made to add the Desprite functionality, to separate the images
    frim sprite image

--- demo1.js
+++ demo1.js
@@ -10,18 +10,18 @@ function mergeImg(){
 	
 	imageObj1.width = imageObj2.width = imageObj3.width = imageObj4.width = "50";
 	imageObj1.height = imageObj2.height = imageObj3.height = imageObj4.height = "50";
-	imageObj1.src = "resources/images/PDF.JPG"
-	imageObj2.src = "resources/images/PPT.JPG";
-	imageObj3.src = "resources/images/FILE.JPG";
-	imageObj4.src = "resources/images/DOC.JPG";
+	imageObj1.src = "images/PDF.JPG"
+	imageObj2.src = "images/PPT.JPG";
+	imageObj3.src = "images/FILE.JPG";
+	imageObj4.src = "images/DOC.JPG";
 	console.log(imageObj1);
 	console.log(imageObj2);
 	console.log(imageObj3);
 	console.log(imageObj4);
 	ctx.drawImage(imageObj1, 0, 0, 50, 50);
-	ctx.drawImage(imageObj2, 40, 0, 50, 50);
-	ctx.drawImage(imageObj3,80,0,50,50);
-	ctx.drawImage(imageObj4, 120, 0, 50, 50);
+	ctx.drawImage(imageObj2, 50, 0, 50, 50);
+	ctx.drawImage(imageObj3,100,0,50,50);
+	ctx.drawImage(imageObj4, 150, 0, 50, 50);
 	/*imageObj1.onload = function() {
 	   ctx.drawImage(imageObj1, 0, 0, 50, 50);
 	   imageObj2.onload = function() {
@@ -39,13 +39,33 @@ function mergeImg(){
 	};*/
 	
 }
+function sprite (options){
+	var obj = {};
+	console.log(options.context);	
+	//options.context.clear(true);
+	obj.context = options.context;
+	obj.height = options.height;
+	obj.width = options.width;
+	obj.image = options.image;
+        // Draw the animation
+	
+	obj.context.drawImage(obj.image,0,0,50,50,0,0,obj.width,obj.height);
+	
+	//return obj;
+	
+}
+
 function demergeImg(){
-	//console.log(ctx.getImageData(0,0,40,40));
-	var img1 = new Image();
-	ctx.putImageData = ctx.getImageData(0,0,40,40);
-	c.appendChild(ctx);
-	console.log(ctx);
-	//ctx.drawImage(ctx.getImageData(0,0,40,40));
-	//var img1 = ctx.drawImage()
+	var spriteImage = new Image();
+	spriteImage.src = "images/download.png";
+	var canvas = document.getElementById("mycanvas");
+	var context1 = canvas.getContext("2d");
+	context1.clearRect(0, 0, canvas.width, canvas.height);
+	var img = sprite({
+		context : context1,
+		width : 100,
+		height : 100,
+		image : spriteImage
+	})
 	
 }
